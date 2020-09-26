6b661d4e1a1c353aee040da09bb8d5eb626b10e9
ddalkigum <sol35352000@gmail.com>
2020-09-26 20:17:36 +0900

    deploy-v2

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = "b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "http://ddalki-movie.eba-q5xst3em.ap-northeast-2.elasticbeanstalk.com/"
-]
+ALLOWED_HOSTS = ["ddalki-movie.eba-q5xst3em.ap-northeast-2.elasticbeanstalk.com"]
 
 
 # Application definition

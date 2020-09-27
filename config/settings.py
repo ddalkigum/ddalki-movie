05eb914d8b345844c2f0c516903ae206b7f55e17
ddalkigum <sol35352000@gmail.com>
2020-09-27 11:44:15 +0900

    ver17

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-if DEBUG is False:
+if DEBUG:
 
     DATABASES = {
         "default": {

0f42439bea48c9f1784e084544e0b86566c85e93
ddalkigum <sol35352000@gmail.com>
2020-09-27 12:39:32 +0900

    ver18

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "b#s*_o(3t3ai_k(c5po@h7a=nj5#vjkd3u7ckhnx@)mi=8fn67"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ddalki-movie.eba-q5xst3em.ap-northeast-2.elasticbeanstalk.com"]
 
